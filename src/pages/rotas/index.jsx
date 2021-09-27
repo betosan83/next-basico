913a87fe3090542bdb769a0126944c0a1cd32c38
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-09-27 19:48:06 -0300

    First commit!

---
+++ src/pages/rotas/index.jsx
@@ -0,0 +1,10 @@
+export default function rotas() {
+    return (
+        <div>
+            <h1>Rotas</h1>
+            <ul>
+                <li>Params</li>
+            </ul>
+        </div>
+    )
+}
