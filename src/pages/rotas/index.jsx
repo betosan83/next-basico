2516d77f6a0d8049bd4309c600820f10e083706b
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-09-28 06:44:42 -0300

    Navegando entre páginas.

--- src/pages/rotas/index.jsx
+++ src/pages/rotas/index.jsx
@@ -1,6 +1,19 @@
 import Link from 'next/link'
+import router from "next/router";
 
 export default function rotas() {
+    function navegacaoSimples(url) {
+        router.push(url)
+    }
+    function navegacaoComParams() {
+        router.push({
+            pathname: "rotas/params",
+            query: {
+                id: 123,
+                nome: 'Ana'
+            }
+        })
+    }
     return (
         <div>
             <h1>Rotas</h1>
@@ -14,8 +27,12 @@ export default function rotas() {
                 <Link href="/rotas/123/Roberto" passHref={true}>
                     <li>Roberto</li>
                 </Link>
-                
             </ul>
+            <div style={{ display:"flex", flexDirection: "column", alignItems: "flex-start"}}>
+                <button onClick={navegacaoComParams}>Params</button>
+                <button onClick={() => router.push("/rotas/123/buscar")}>Buscar</button>
+                <button onClick={() => navegacaoSimples("/rotas/123/Roberto")}>Roberto</button>
+            </div>
         </div>
     )
 }
