7065e0c69d8234e9f3cadf9d046c62b6464fe0bd
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-10-02 21:37:47 -0300

    Limpando formulário.

--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -11,6 +11,9 @@ export default function Form() {
             body: JSON.stringify({nome, idade})
         })
 
+        setNome("")
+        setIdade(0)
+
         const resp = await fetch('/api/form')
         const usuarios = await resp.json()
         setUsuarios(usuarios)
